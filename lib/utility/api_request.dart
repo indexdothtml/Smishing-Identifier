45905103489d160751ef21d1732549ff0dd04392
Abhishek Kshirsagar <abhi.kshirsagar1100@gmail.com>
2022-02-06 01:28:40 +0530

    Bugs fixed - The null response problem is fixed it was getting when URL is Safe

--- lib/utility/api_request.dart
+++ lib/utility/api_request.dart
@@ -3,6 +3,10 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:smishing_identifier_application/utility/storage_handler.dart';
+
+AppDataStorageManager appDataStorageManager = AppDataStorageManager();
+
 Future<String> makeRequest(requestedUrl) async {
   final url = Uri.parse(
       "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyAvPztv_OzAJojxmHvNyi1De20qzubMLaY");
@@ -19,11 +23,12 @@ Future<String> makeRequest(requestedUrl) async {
     }
   });
 
-  try {
-    final response = await http.post(url, headers: header, body: body);
-    final jsonData = jsonDecode(response.body);
+  final response = await http.post(url, headers: header, body: body);
+  final jsonData = jsonDecode(response.body);
+  if (jsonData.toString() == '{}') {
+    return "SAFE";
+  } else {
+    appDataStorageManager.writeToFile(requestedUrl);
     return jsonData["matches"][0]["threatType"];
-  } catch (error) {
-    return "Network Error !";
   }
 }
