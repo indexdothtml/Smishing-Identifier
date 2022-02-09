6d383fefe5af838d818827d25139c4a5d7f9af33
Abhishek Kshirsagar <abhi.kshirsagar1100@gmail.com>
2022-02-10 00:40:58 +0530

    Dialog box UI modified, sms body and address display added with warning icon or safe icon respect with final result output.

--- lib/pages/sms_dialog.dart
+++ lib/pages/sms_dialog.dart
@@ -4,30 +4,83 @@ import 'package:smishing_identifier_application/utility/utility_controller.dart'
 UtilityController utilityController = UtilityController();
 
 showSMSDialog(context, messages, String responseResult, index) {
-
   if (responseResult == "UNSAFE") {
     return showDialog(
         context: context,
         builder: (context) {
-          return const Dialog(
-            backgroundColor: Colors.grey,
-            child: Icon(
-              Icons.warning,
-              color: Colors.redAccent,
-            ),
-          );
+          return Dialog(
+              backgroundColor: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(8.5),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(
+                      Icons.warning_amber_rounded,
+                      color: Colors.redAccent,
+                      size: 35,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      messages[index].address,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      messages[index].body,
+                      style: const TextStyle(
+                        fontSize: 15,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
+                ),
+              ));
         });
   } else {
     return showDialog(
         context: context,
         builder: (context) {
-          return const Dialog(
-            backgroundColor: Colors.grey,
-            child: Icon(
-              Icons.check,
-              color: Colors.greenAccent,
-            ),
-          );
+          return Dialog(
+              backgroundColor: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(8.5),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(
+                      Icons.check_circle_outline_rounded,
+                      color: Colors.green,
+                      size: 35,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      messages[index].address,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      messages[index].body,
+                      style: const TextStyle(
+                        fontSize: 15,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
+                ),
+              ));
         });
   }
 }
