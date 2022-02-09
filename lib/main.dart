64914b05033fdb69dd57e263a2fcf04b6ec13afa
Abhishek Kshirsagar <abhi.kshirsagar1100@gmail.com>
2022-02-10 00:36:06 +0530

    prefered const keyword added

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home : SMSInbox(),
+      home : const SMSInbox(),
     );
   }
 }
