12730c65c06ad05e0f1789b405dc1d062845fe41
Abhishek Kshirsagar <abhi.kshirsagar1100@gmail.com>
2022-02-09 21:28:27 +0530

    Condition is used for display result is removed due to sync issue with calculated result. onTap method is added which contains fromExtracter function call and showSMSDialog function call which gives parameter as result got from fromExtracter after all process.

--- lib/pages/my_inbox.dart
+++ lib/pages/my_inbox.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:smishing_identifier_application/utility/extracter.dart';
+import 'package:smishing_identifier_application/pages/sms_dialog.dart';
+import 'package:smishing_identifier_application/utility/utility_controller.dart';
 import 'package:telephony/telephony.dart';
-
-String responseResult = "";
 
 class SMSInbox extends StatefulWidget {
   const SMSInbox({Key? key}) : super(key: key);
@@ -13,8 +12,9 @@ class SMSInbox extends StatefulWidget {
 
 class _SMSInboxState extends State<SMSInbox> {
   final Telephony telephony = Telephony.instance;
+  UtilityController utilityController = UtilityController();
 
-  List<SmsMessage> messages = [];
+  Map<int, String> safeUnsafeData = {};
 
   @override
   void initState() {
@@ -36,37 +36,14 @@ class _SMSInboxState extends State<SMSInbox> {
         ),
       ),
       body: FutureBuilder(
-        future: chkInbox(),
-        builder: (context, snapshot) {
-          return ListView.separated(
-            separatorBuilder: (context, index) => Divider(
-              color: Colors.black,
-            ),
-            itemCount: messages.length,
-            itemBuilder: (context, index) {
-              dynamic fromExtractor() async {
-                responseResult =
-                    await extractLink(messages[index].body.toString());
-              }
-
-              fromExtractor();
-              print(responseResult);
-              if (responseResult == "MALWARE") {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.warning,
-                      color: Colors.redAccent,
-                    ),
-                    title: Text(messages[index].address.toString()),
-                    subtitle: Text(
-                      messages[index].body.toString(),
-                    ),
-                    style: ListTileStyle.drawer,
-                  ),
-                );
-              } else {
+          future: utilityController.chkInbox(),
+          builder: (context, snapshot) {
+            return ListView.separated(
+              separatorBuilder: (context, index) => const Divider(
+                color: Colors.black,
+              ),
+              itemCount: utilityController.messages.length,
+              itemBuilder: (context, index) {
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ListTile(
@@ -74,28 +51,83 @@ class _SMSInboxState extends State<SMSInbox> {
                       Icons.textsms,
                       color: Colors.deepPurple,
                     ),
-                    title: Text(messages[index].address.toString()),
+                    title: Text(
+                        utilityController.messages[index].address.toString()),
                     subtitle: Text(
-                      messages[index].body.toString(),
+                      utilityController.messages[index].body.toString(),
+                      maxLines: 3,
                     ),
                     style: ListTileStyle.drawer,
+                    onTap: () async {
+                      String responseResult =
+                          await utilityController.fromExtracter(index);
+                      showSMSDialog(context, utilityController.messages,
+                          responseResult, index);
+                      // callFromExtracterFunction(index) async {
+
+                      // }
+                    },
                   ),
                 );
-              }
-            },
-          );
-        },
-      ),
-    );
-  }
+              },
+            );
+          }),
 
-  chkInbox() async {
-    messages = await telephony.getInboxSms(columns: [
-      SmsColumn.ADDRESS,
-      SmsColumn.BODY,
-    ], sortOrder: [
-      OrderBy(SmsColumn.ID, sort: Sort.DESC),
-    ]);
+      // body: ListView.separated(
+      //   separatorBuilder: (context, index) => const Divider(
+      //     color: Colors.black,
+      //   ),
+      //   itemCount: utilityController.messages.length,
+      //   itemBuilder: (context, index) {
+      //     // callFromExtracterFunction(messages, index);
+
+      //     print(
+      //         "2) I AM INBOX, AND I GOT THE FINAL RESULT FROM EXTRACTER : ${safeUnsafeData[index]} FOR INDEX $index"); //DEBUG
+      //     if (safeUnsafeData[index] == "UNSAFE") {
+      //       return Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: ListTile(
+      //           leading: const Icon(
+      //             Icons.warning,
+      //             color: Colors.redAccent,
+      //           ),
+      //           title:
+      //               Text(utilityController.messages[index].address.toString()),
+      //           subtitle: Text(
+      //             utilityController.messages[index].body.toString(),
+      //           ),
+      //           trailing: Text(index.toString()),
+      //           style: ListTileStyle.drawer,
+      //         ),
+      //       );
+      //     } else {
+      //       return Padding(
+      //         padding: const EdgeInsets.all(8.0),
+      //         child: ListTile(
+      //           leading: const Icon(
+      //             Icons.textsms,
+      //             color: Colors.deepPurple,
+      //           ),
+      //           title:
+      //               Text(utilityController.messages[index].address.toString()),
+      //           subtitle: Text(
+      //             utilityController.messages[index].body.toString(),
+      //           ),
+      //           trailing: Text(index.toString()),
+      //           style: ListTileStyle.drawer,
+      //         ),
+      //       );
+      //     }
+      //   },
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     print(
+      //         "CALLING SICONTROLLER FUNCTION INSIDE UTILITY CONTROLLER CLASS");
+      //     safeUnsafeData = utilityController.siController();
+      //   },
+      // ),
+    );
   }
 
   onMessage(SmsMessage newMessage) async {
