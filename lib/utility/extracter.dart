cdc376e4804d605ce03110b54dac9cc3234cb70e
Abhishek Kshirsagar <abhi.kshirsagar1100@gmail.com>
2022-02-14 16:04:11 +0530

    Debug functions removed and more comparison keywords added.

--- lib/utility/extracter.dart
+++ lib/utility/extracter.dart
@@ -3,7 +3,6 @@ import 'package:smishing_identifier_application/utility/storage_handler.dart';
 AppDataStorageManager appDataStorageManager = AppDataStorageManager();
 
 Future<String> extractLink(String givenbody) async {
-  print("1) I GOT THIS BODY : $givenbody");
   String url = "";
 
   givenbody = givenbody.replaceAll("\n", ' ');
@@ -15,21 +14,18 @@ Future<String> extractLink(String givenbody) async {
     if (keywords[i].contains('.com') ||
         keywords[i].contains('.in') ||
         keywords[i].contains('.org') ||
+        keywords[i].contains('.co') ||
+        keywords[i].contains('http://') ||
+        keywords[i].contains('https://') ||
         keywords[i].contains('.xyz')) {
       url = keywords[i];
       break;
     }
   }
   if (url.isNotEmpty) {
-    print("2) I AM EXTRACTER, I GOT THE URL INSIDE BODY : $url");
-    print(
-        "3) I AM EXTRACTER, NOW I AM SENDING THIS URL TO STORAGE READER : $url");
     String resultFromStorage = await appDataStorageManager.readFromFile(url);
-    print(
-        "4) I AM EXTRACTER, I GOT THE RESULT FROM STORAGE : $resultFromStorage FOR URL $url");
     return resultFromStorage;
   } else {
-    print("2) I AM EXTRACTER, NOT FOUND ANY URL INSIDE BODY");
     return "No Link Found";
   }
 }
