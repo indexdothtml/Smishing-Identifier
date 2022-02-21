746253e70ac707efd28ece4ab1d5cf5caf9fe3b6
Abhishek Kshirsagar <abhi.kshirsagar1100@gmail.com>
2022-02-22 00:17:57 +0530

    URL extraction improved

--- lib/utility/extracter.dart
+++ lib/utility/extracter.dart
@@ -11,17 +11,35 @@ Future<String> extractLink(String givenbody) async {
   List<String> keywords = givenbody.split(' ');
 
   for (int i = 0; i < keywords.length; i++) {
-    if (keywords[i].contains('.com') ||
-        keywords[i].contains('.in') ||
-        keywords[i].contains('.org') ||
-        keywords[i].contains('.ly') ||
-        keywords[i].contains('.at') ||
-        keywords[i].contains('http://') ||
-        keywords[i].contains('https://') ||
-        keywords[i].contains('.xyz')) {
-      url = keywords[i];
-      break;
-    }
+    if (keywords[i].contains('.a') ||
+        keywords[i].contains('.b') ||
+        keywords[i].contains('.c') ||
+        keywords[i].contains('.d') ||
+        keywords[i].contains('.e') ||
+        keywords[i].contains('.f') ||
+        keywords[i].contains('.g') ||
+        keywords[i].contains('.h') ||
+        keywords[i].contains('.i') ||
+        keywords[i].contains('.j') ||
+        keywords[i].contains('.k') ||
+        keywords[i].contains('.l') ||
+        keywords[i].contains('.m') ||
+        keywords[i].contains('.n') ||
+        keywords[i].contains('.o') ||
+        keywords[i].contains('.p') ||
+        keywords[i].contains('.q') ||
+        keywords[i].contains('.r') ||
+        keywords[i].contains('.s') ||
+        keywords[i].contains('.t') ||
+        keywords[i].contains('.u') ||
+        keywords[i].contains('.v') ||
+        keywords[i].contains('.w') ||
+        keywords[i].contains('.x') ||
+        keywords[i].contains('.y') ||
+        keywords[i].contains('.z')) {
+          url = keywords[i];
+          break;
+        }
   }
   if (url.isNotEmpty) {
     String resultFromStorage = await appDataStorageManager.readFromFile(url);
